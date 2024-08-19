88460eb8360ce653a083378d400c7619ab3d3d59
zero_jeden <zeroitjeden@gmail.com>
2024-08-19 12:48:38 +0200

    fix: try use function inside object

--- task-1/script.js
+++ task-1/script.js
@@ -1,20 +1,36 @@
-const inc = function () {
+const makeCounter = function (name) {
+	return {
+		name: name,
+		number: 0,
+		inc: makeCounter.inc,
+		stat: makeCounter.stat,
+	};
+};
+
+//this is test functions inside object
+//this is not the best solution
+//constructor is better
+
+makeCounter.inc = function () {
 	++this.number;
 };
 
-const stat = function () {
+makeCounter.stat = function () {
 	console.log(`info from: ${this.name}. current number is: ${this.number}`);
-};
-const counter1 = {
-	name: 'counter1',
-	number: 0,
-	inc: inc,
-	stat: stat,
+	return this.number;
 };
 
-const counter2 = {
-	name: 'counter2',
-	number: 0,
-	inc: inc,
-	stat: stat,
-};
+const counter1 = makeCounter('counter1');
+const counter2 = makeCounter('counter2');
+const counter3 = makeCounter('counter3');
+
+counter1.inc();
+counter1.inc();
+counter1.inc();
+counter2.inc();
+counter2.inc();
+counter3.inc();
+
+if (counter1.stat() !== 3) throw new Error('counter 1 do not work');
+if (counter2.stat() !== 2) throw new Error('counter 2 do not work');
+if (counter3.stat() !== 1) throw new Error('counter 3 do not work');

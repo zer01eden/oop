c03bcb4ffb798b8a7aae5f07b5b2db1a69ec228e
zero_jeden <zeroitjeden@gmail.com>
2024-08-20 10:58:34 +0200

    fix: use construktor & prototype & keyword new

---
+++ task-2/script.js
@@ -0,0 +1,34 @@
+
+// test constructor function 
+const Counter = function (name) {
+	this.name = name, 
+	this.number = 0
+};
+
+// test prototype 
+Counter.prototype.inc = function () {
+	++this.number;
+};
+
+Counter.prototype.stat = function () {
+	console.log(`info from: ${this.name}. current number is: ${this.number}`);
+	return this.number;
+};
+
+// keyword *new* use function Counter created object 
+const counter1 = new Counter('counter1');
+const counter2 = new Counter('counter2');
+const counter3 = new Counter('counter3');
+
+// tests ;)
+counter1.inc();
+counter1.inc();
+counter1.inc();
+counter2.inc();
+counter2.inc();
+counter3.inc();
+
+
+if (counter1.stat() !== 3) throw new Error('counter 1 do not work');
+if (counter2.stat() !== 2) throw new Error('counter 2 do not work');
+if (counter3.stat() !== 1) throw new Error('counter 3 do not work');
